33cad7588bfb53d899b3f4053906e9dafc9891de
JJMG <JJMGlabs@gmail.com>
2022-08-25 23:37:52 +0100

    Add blazor template code

---
+++ BlazorMultiPlatformTemplate/WpfBlazorApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.Data;
+
+namespace WpfBlazorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
